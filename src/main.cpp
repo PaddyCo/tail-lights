393ea57b99781914b7b30a6db25be6090c3677cc
Patrik Riström <patrik@timelab.se>
2021-11-25 14:27:20 +0100

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,34 @@
 #define BLINKER_LEFT_PIN 51
 #define BLINKER_RIGHT_PIN 50
 #define BRAKE_PIN 52
-#define NUM_LEDS 30
+#define NUM_LEDS 31
 #define COLOR_ORDER BGR
 #define GLOBAL_BRIGHTNESS 255
-#define FRAME_TIME 16
+#define FRAME_TIME 8
+#define BLINKER_WIDTH 15
+#define CENTER_POSITION NUM_LEDS-1
+#define NUMBER_OF_GLIDERS 200 
+#define BLINKER_PULSE_COOLDOWN 500
 
 CRGB leds[NUM_LEDS];
 uint32_t prevFrameTime;
 uint32_t startupAnimationProgress;
-uint32_t brakeLightAnimationProgress;
-uint8_t brakeLightIndex = 0;
-uint8_t brakeLightLed [200] = { }; // TODO: Figure out a reasonable max limit?
+int32_t leftBlinkerPulseCooldown;
+int32_t rightBlinkerPulseCooldown;
+uint8_t glidersIndex = 0;
+
+enum GliderType {
+    None,
+    Brake,
+    Blinker
+};
+
+struct Glider {
+    uint8_t position;
+    GliderType type;
+};
+
+Glider gliders [NUMBER_OF_GLIDERS] = { }; // TODO: Figure out a reasonable max limit?
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -24,18 +41,20 @@ void setup() {
 
     pinMode(50, INPUT);
 
-    brakeLightAnimationProgress = 0;
     startupAnimationProgress = 0;
+    leftBlinkerPulseCooldown = 0;
+    rightBlinkerPulseCooldown = 0;
 
     // Clear LEDs
     FastLED.clear();
     FastLED.show();
 
     // TODO: Remove this when done, this is just to test the startup animation
-    delay(1000);
+    delay(200);
 
-    for(int i = 0; i < sizeof(brakeLightLed); i++) { 
-        brakeLightLed[i] = NUM_LEDS+1;
+    for(uint32_t i = 0; i < NUMBER_OF_GLIDERS; i++) { 
+        Glider nullGlider { .position = 0, .type = None };
+        gliders[i] = nullGlider;
     }
 
     prevFrameTime = millis();
@@ -58,76 +77,171 @@ void startupAnimation(uint32_t deltaTime) {
 }
 
 #define BRAKE_LIGHT_ANIMATION_SPEED 10
-void brakeLightAnimation(uint32_t deltaTime) {
-    uint8_t centerLed = NUM_LEDS/2;
+void updateBrakeGliders(uint32_t deltaTime, bool reverse) {
+    CRGB colors [NUM_LEDS];
+    for (int i = 0; i < NUM_LEDS; i++) {
+        colors[i] = CRGB::Black;
+    }
     CRGB color = CRGB::Red;
-    for (int i = 0; i < sizeof(brakeLightLed); i++) {
-        uint8_t led = brakeLightLed[i];
-        if (led < 0 || led > NUM_LEDS) {
+    CRGB fadedColor = CRGB(128, 0, 0); //color.fadeToBlackBy(100);
+    for (int i = 0; i < NUMBER_OF_GLIDERS; i++) {
+        Glider glider = gliders[i];
+        if (glider.type != Brake) {
             continue;
         }
 
-        leds[led] = color;
+        if (glider.position > (NUM_LEDS-1)*2) { continue; }
+        if (glider.position < 0) { continue; }
+        if (glider.position == CENTER_POSITION && reverse) { 
+            gliders[i].type = None;
+        }
 
-        if (led >= centerLed) { 
-            brakeLightLed[i] += 1;
-        } else if (led < centerLed) {
-            brakeLightLed[i] -= 1;
+        //if (reverse && led == centerLed) {
+        //    led = NUM_LEDS+1;
+        //    continue;
+        //}
+        //
+
+        if (glider.position % 2 == 0) {
+            colors[glider.position/2] = color;
+        } else {
+            uint8_t pos = glider.position/2;
+            if (colors[pos].getLuma() == 0) {
+                colors[pos] = fadedColor;
+            }
+
+            if (colors[pos+1].getLuma() == 0) {
+                colors[pos+1] = fadedColor;
+            }
+        }
+
+        uint8_t speed = 1;
+        if (glider.position > CENTER_POSITION) { 
+            gliders[i].position += (reverse ? -1 : 1) * speed;
+        } else if (glider.position < CENTER_POSITION) {
+            gliders[i].position += (reverse ? 1 : -1) * speed;
         }
     }
-    //if (digitalRead(BRAKE_PIN)) {
-    //    brakeLightAnimationProgress = 0;
-    //    return;
-    //}
 
-    //brakeLightAnimationProgress += deltaTime;
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+        if (colors[i].getLuma() == 0) { continue; }
+        leds[i] = colors[i];
+    }
+}
 
-    //CRGB color = CRGB::Red;
-    //uint8_t centerLed = NUM_LEDS/2;
+#define BLINKERS_LIGHT_ANIMATION_SPEED 10
+void updateBlinkerGliders(uint32_t deltaTime) {
+    CRGB colors [NUM_LEDS];
+    for (int i = 0; i < NUM_LEDS; i++) {
+        colors[i] = CRGB::Black;
+    }
+    CRGB color = CRGB::OrangeRed;
+    CRGB fadedColor = color.fadeToBlackBy(100);
+    for (int i = 0; i < NUMBER_OF_GLIDERS; i++) {
+        Glider glider = gliders[i];
+        if (glider.type != Blinker) {
+            continue;
+        }
 
-    //for(int dot = 0; dot < NUM_LEDS; dot++) { 
-    //    uint8_t offset = abs(centerLed - dot);
-    //    if (offset < brakeLightAnimationProgress/BRAKE_LIGHT_ANIMATION_SPEED) {
-    //        leds[dot] = color; 
-    //    }
-    //}
+        if (glider.position < 0 || glider.position > (NUM_LEDS-1)*2) { 
+            gliders[i].type = None;
+            continue; 
+        }
+
+        if (glider.position % 2 == 0) {
+            colors[glider.position/2] = color;
+        } else {
+            uint8_t pos = glider.position/2;
+            if (colors[pos].getLuma() == 0) {
+                colors[pos] = fadedColor;
+            }
+
+            if (colors[pos+1].getLuma() == 0) {
+                colors[pos+1] = fadedColor;
+            }
+        }
+
+        uint8_t speed = 1;
+        if (glider.position > CENTER_POSITION) { 
+            gliders[i].position += (1 * speed);
+        } else if (glider.position < CENTER_POSITION) {
+            gliders[i].position += (-1 * speed);
+        }
+    }
+
+    for (uint8_t i = 0; i < NUM_LEDS; i++) {
+        if (colors[i].getLuma() == 0) { continue; }
+        leds[i] = colors[i];
+    }
+}
+
+void spawnGlider(uint8_t position, GliderType type) {
+    Glider newGlider { .position = position, .type = type };
+    gliders[glidersIndex] = newGlider;
+    glidersIndex++;
+
+    if (glidersIndex > NUMBER_OF_GLIDERS-1) {
+        glidersIndex = 0;
+    }
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-
     uint32_t deltaTime = millis() - prevFrameTime;
+    Serial.println(1000 / deltaTime);
 
     FastLED.setBrightness(GLOBAL_BRIGHTNESS);
 
-    if (!digitalRead(BRAKE_PIN)) {
-        uint8_t centerLed = NUM_LEDS/2;
-        
-        brakeLightLed[brakeLightIndex] = centerLed;
-        brakeLightIndex++;
-        brakeLightLed[brakeLightIndex] = centerLed-1;
-        brakeLightIndex++;
+    startupAnimation(deltaTime);
 
-        if (brakeLightIndex > sizeof(brakeLightLed)-1) {
-            brakeLightIndex = 0;
+    if (!digitalRead(BRAKE_PIN)) {
+        spawnGlider(CENTER_POSITION-1, Brake);
+        spawnGlider(CENTER_POSITION, Brake);
+        spawnGlider(CENTER_POSITION+1, Brake);
+        updateBrakeGliders(deltaTime, false);
+    } else {
+        updateBrakeGliders(deltaTime, true);
+    }
+
+
+
+
+    CRGB blinkerColor = CRGB::OrangeRed;
+
+    if (!digitalRead(BLINKER_LEFT_PIN) && leftBlinkerPulseCooldown <= 0) {
+        leftBlinkerPulseCooldown = BLINKER_PULSE_COOLDOWN;
+    }
+
+    if (!digitalRead(BLINKER_RIGHT_PIN) && rightBlinkerPulseCooldown <= 0) {
+        rightBlinkerPulseCooldown = BLINKER_PULSE_COOLDOWN;
+    }
+    if (leftBlinkerPulseCooldown > 0) { leftBlinkerPulseCooldown -= deltaTime; }
+    if (rightBlinkerPulseCooldown > 0) { rightBlinkerPulseCooldown -= deltaTime; }
+
+    if (leftBlinkerPulseCooldown > BLINKER_PULSE_COOLDOWN * 0.8) {
+        spawnGlider(BLINKER_WIDTH, Blinker);
+    }
+
+    if (rightBlinkerPulseCooldown > BLINKER_PULSE_COOLDOWN * 0.8) {
+        spawnGlider(((NUM_LEDS-1)*2)-BLINKER_WIDTH, Blinker);
+    }
+
+    if (rightBlinkerPulseCooldown > 0) {
+        uint8_t maxPos = (NUM_LEDS-1)*2;
+        for (uint8_t i = (maxPos-BLINKER_WIDTH); i <= maxPos; i++) {
+            leds[i/2] = CRGB::OrangeRed;
+            leds[i/2].fadeToBlackBy(250);
         }
     }
 
-    startupAnimation(deltaTime);
-    brakeLightAnimation(deltaTime);
-    CRGB blinkerColor = CRGB::Yellow;
-
-    if (!digitalRead(BLINKER_LEFT_PIN)) {
-        leds[0] = blinkerColor;
-        leds[1] = blinkerColor;
-        leds[2] = blinkerColor;
+    if (leftBlinkerPulseCooldown > 0) {
+        for (uint8_t i = 0; i < (BLINKER_WIDTH/2)+2; i++) {
+            leds[i] = CRGB::OrangeRed;
+            leds[i].fadeToBlackBy(250);
+        }
     }
 
-    if (!digitalRead(BLINKER_RIGHT_PIN)) {
-        leds[NUM_LEDS-1] = blinkerColor;
-        leds[NUM_LEDS-2] = blinkerColor;
-        leds[NUM_LEDS-3] = blinkerColor;
-    }
+    updateBlinkerGliders(deltaTime);
 
     FastLED.show(); 
 
